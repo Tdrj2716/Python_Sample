843380c27699cc739bd877f197acb8b8127bdd29
Tdrj2716 <ri76joy@gmail.com>
2020-08-23 13:43:50 +0900

    type and id
    rename programs

---
+++ Sample1.py
@@ -0,0 +1,16 @@
+# learn variables and data types
+
+a = 1
+b = 2
+print(a)
+c = a + b
+print(c)
+c = 4
+print(c)
+s = "python"
+print(s)
+
+print(type(c), type(s))
+
+print(id(c))
+print(id(s))
--- variable.py
+++
@@ -1,9 +0,0 @@
-a = 1
-b = 2
-print(a)
-c = a + b
-print(c)
-c = 4
-print(c)
-s = "python"
-print(s)
